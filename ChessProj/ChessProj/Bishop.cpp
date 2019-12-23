e98eb042615b7ba182b90da0de109832e1b78686
Ofir Shapira <ofirs2003@gmail.com>
2019-12-24 01:17:04 +0200

    add iSValidMove definition to Bishop.cpp (just the definition not the real code)

--- ChessProj/ChessProj/Bishop.cpp
+++ ChessProj/ChessProj/Bishop.cpp
@@ -4,5 +4,9 @@ Bishop::Bishop(string color, Position pos) : Piece(color, "Bishop", pos)
 {
 }
 Bishop::~Bishop() = default;
+const bool Bishop::isValidMove(const Piece * table[][TABLE_SIZE], Position dest)
+{
+	return true; // need to be done
+}
 //virtual void buildMoveOptions(const Piece*  board[][TABLE_SIZE]);
  const bool isValidMove(const Piece* table[][TABLE_SIZE], Position dest);
