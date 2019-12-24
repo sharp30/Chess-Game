afaa86680c66340026af8c995301d9b58f8ab2cb
Ofir Shapira <ofirs2003@gmail.com>
2019-12-25 01:37:16 +0200

    added Knight.cpp and Knight.h  + and finish them

---
+++ ChessProj/ChessProj/Knight.cpp
@@ -0,0 +1,21 @@
+#include "Knight.h"
+#include <cmath>
+// --------------- constractor ---------------------------------
+Knight::Knight(string color, Position pos):Piece(color,"Knight",pos)
+{
+}
+//-------------- distractor -----------------------------------
+Knight::~Knight() = default;
+
+/*
+This function checks if the object can execute the move --- according to Queen's moving rules + if the path to dest is empty
+Input:table -> the board of the game :: Piece* [][TABLE_SIZE]
+	  dest -> the destination to move to :: Position
+Output:Is the move valid? :: bool
+*/
+bool Knight::isValidMove(const Piece* table[][TABLE_SIZE], Position dest) const
+{
+	int disRow = abs((int)(this->_pos.getRow() - dest.getRow()));
+	int disCol = abs((int)(this->_pos.getCol() - dest.getCol()));
+	return disRow > 0 && disCol > 0 && (disRow + disCol) == 3;
+ }
---
+++ ChessProj/ChessProj/Knight.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Piece.h"
+
+
+using std::string;
+
+
+class Queen : public Piece
+{
+public:
+	Queen(string color, Position pos);
+	~Queen();
+	//virtual void buildMoveOptions(const Piece*  board[][TABLE_SIZE]);
+	virtual bool isValidMove(const Piece* table[][TABLE_SIZE], Position dest) const;
+};
